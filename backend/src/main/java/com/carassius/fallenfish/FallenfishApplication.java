9953ed0ada28086f206285cf640ba861e8f73813
이희수 <2heedol@naver.com>
2023-04-21 15:51:16 +0900

    init backend project

---
+++ backend/src/main/java/com/carassius/fallenfish/FallenfishApplication.java
@@ -0,0 +1,13 @@
+package com.carassius.fallenfish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FallenfishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FallenfishApplication.class, args);
+	}
+
+}
