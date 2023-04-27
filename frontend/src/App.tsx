0de338f15b4ae87f276f04374f567a1bd3d8948d
김지훈 <ji_hun23@naver.com>
2023-04-27 09:44:47 +0900

    feat:싱글게임페이지 초기세팅

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Routes } from 'react-router';
 import { Route, Outlet } from 'react-router-dom';
+import SingleGamePage from './present/pages/SingleGamePage';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" />
-      <Route path="/game" element={<Outlet />}>
-        <Route index />
-        <Route path="/single" />
-        <Route path="/multi" />
-      </Route>
-    </Routes>
+    <React.Fragment>
+      <Routes>
+        <Route path="/" />
+        <Route path="/game" element={<Outlet />}>
+          <Route index />
+          <Route path="/single" element={<SingleGamePage />} />
+          <Route path="/multi" />
+        </Route>
+      </Routes>
+    </React.Fragment>
   );
 }
 
---
+++ frontend/src/present/pages/SingleGamePage.tsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import useLoadScript from "../../action/hooks/useLoadScript";
+
+import SingleDomestic from "../layout/single/SingleDomestic";
+
+const SingleGamePage = () => {
+    // let locationMod = "international";
+    let locationMod = "domestic";
+    const NAVER_API_KEY = `https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=4vgyzjsnlj&submodules=panorama`;
+    const GOOGLE_API_KEY = `https://maps.googleapis.com/maps/api/js?key=AIzaSyC5fl-yV_BZhfIXZYDpU4JnCwFGDhd8oQA`;
+    const [scriptLoadingState] = useLoadScript(locationMod === "domestic" ? NAVER_API_KEY : GOOGLE_API_KEY);
+    const [showMap, setShowMap] = useState(false);
+
+    useEffect(() => {
+        if (scriptLoadingState !== 'l' && scriptLoadingState !== 'e') {
+            setTimeout(() => {
+                setShowMap(true);
+            },1500)
+        }
+    }, [scriptLoadingState]);
+
+    return (
+        <React.Fragment>
+            {
+                showMap ? (locationMod === 'domestic' ? <SingleDomestic /> : <SingleDomestic />) : null
+            }
+        </React.Fragment>
+    )
+}
+
+export default SingleGamePage;
