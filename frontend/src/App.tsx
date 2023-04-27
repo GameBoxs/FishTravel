7711127347b9b2053382e3afbaaea4381f85ddef
김지훈 <ji_hun23@naver.com>
2023-04-27 16:02:48 +0900

    feat:GlobalStyle 적용

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,10 +3,12 @@ import { Routes } from 'react-router';
 import { Route, Outlet } from 'react-router-dom';
 import * as Styled from './App.Styled';
 import SingleGamePage from './present/pages/SingleGamePage';
+import GlobalStyle from './action/GlobalStyle';
 
 function App() {
   return (
     <Styled.AppWrapper>
+      <GlobalStyle />
       <Routes>
         <Route path="/" />
         <Route path="/game" element={<Outlet />}>
