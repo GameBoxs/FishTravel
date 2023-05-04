7f73c91c0d3095d0d286b7882ad7bae184954627
김지훈 <ji_hun23@naver.com>
2023-05-04 15:50:56 +0900

    fix:종료 라운드 버그 픽스

--- frontend/src/present/component/single/ResultMap.tsx
+++ frontend/src/present/component/single/ResultMap.tsx
@@ -47,7 +47,7 @@ const ResultMap = (props:propsType) => {
                 <Style.ResultText>4</Style.ResultText>
             </Style.ResultInfo>
             {
-                currentStage < 4 ? 
+                currentStage == 3 ? 
                 <Style.ResultNextBtn onClick={startStage}>Next</Style.ResultNextBtn>
                 :
                 <Style.ResultNextBtn onClick={moveHome}>Finish</Style.ResultNextBtn>
