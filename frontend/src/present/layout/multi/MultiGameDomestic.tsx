f13e2d88a7702b7bfa6f96fbfb8593959bede3f9
cloudsoswift <magic9282m@gmail.com>
2023-05-09 17:53:22 +0900

    feat: 네이버 지도 도로명 표시 안 되도록 설정 및 게임 결과창 스켈레톤 만듦

--- frontend/src/present/layout/multi/MultiGameDomestic.tsx
+++ frontend/src/present/layout/multi/MultiGameDomestic.tsx
@@ -119,6 +119,9 @@ const StreetViewContent = styled.div`
   width: 100%;
   height: 100%;
   z-index: 0;
+  span {
+    display: none !important;
+  }
 `
 const CustomButton = styled.button`
   border-width: 0px;
---
+++ frontend/src/present/layout/multi/MultiGameResult.tsx
@@ -0,0 +1,10 @@
+type Props = {
+  
+};
+export const MultiGameResult = (props: Props) => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
