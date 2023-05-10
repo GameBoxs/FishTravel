6bb519a387f193b811d38b8ae4ff734a9836c0c9
cloudsoswift <magic9282m@gmail.com>
2023-05-10 17:45:31 +0900

    feat: 라운드 별 결과 페이지(1,2,3등 시상대 및 4위 이하 순위표시) 대강 구현(실제 값들이랑 연결X)

---
+++ frontend/src/present/component/multi/RankingPodiumItem.tsx
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+
+type Props = {
+  rank: number
+};
+export const RankingPodiumItem = ({ rank }: Props) => {
+  return (
+    <PodiumItemContainer rank={rank}>
+      <CircleIcon profileSrc={"https://picsum.photos/250/250"} />
+      <NicknameText>닉네임</NicknameText>
+      <ScoreText>점수</ScoreText>
+    </PodiumItemContainer>
+  );
+};
+
+const PodiumItemContainer = styled.div<{ rank: number }>`
+  display: grid;
+  text-align: center;
+  margin: 0px 1vw;
+  margin-bottom: ${(props) => { return props.rank === 1 ? "75px" : (props.rank === 2 ? "50px" : "25px") }};
+  padding: 8px;
+`
+
+const CircleIcon = styled.div<{profileSrc: string}>`
+  width: 5vw;
+  height: 5vw;
+  border-radius: 50%;
+  border: 4px solid rgba(255, 255, 255);
+  background-image: ${(props)=>`url(${props.profileSrc})`};
+  object-fit: contain;
+`
+
+const NicknameText = styled.span`
+color: rgba(255, 255, 255);
+  font-size: large;
+`
+
+const ScoreText = styled.span`
+  margin-right: 10px;
+  color: rgba(255, 255, 255, 0.8);
+`
--- frontend/src/present/layout/multi/MultiGameResult.tsx
+++ frontend/src/present/layout/multi/MultiGameResult.tsx
@@ -1,10 +1,71 @@
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import { RankingMap } from "../../component/multi/RankingMap";
+import { RankingPodium } from "../../component/multi/RankingPodium";
+import { ResultRankingItemList } from "../../component/multi/ResultRankingItemList";
+
 type Props = {
-  
+  isDomestic: boolean;
+  isLoaded: string;
 };
-export const MultiGameResult = (props: Props) => {
+export type LatLng = {
+  lat: number;
+  lng: number;
+}
+export const MultiGameResult = ({ isDomestic, isLoaded }: Props) => {
+  const [answerPosition, setAnswerPosition] = useState({ lat: 37.3599605, lng: 127.1058814 });
+  const [selectedPosition, setSelectedPosition] = useState({ lat: 37.3599605, lng: 127.1058814 });
   return (
     <div>
-      
+      <ResultContainer>
+        <ResultContent>
+          <ResultMapContent>
+            <RankingMap id="map" answerPosition={answerPosition} selectedPosition={selectedPosition} />
+          </ResultMapContent>
+          <ResultRankingContent>
+            <RankingPodium />
+            <ResultRankingItemList />
+          </ResultRankingContent>
+        </ResultContent>
+      </ResultContainer>
     </div>
   );
 };
+
+const ResultContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: #4158D0;
+  background-image: linear-gradient(43deg, #4158D0 0%, #C850C0 46%, #FFCC70 100%);
+`
+
+const ResultContent = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translateX(-50%) translatey(-50%);
+  width: 50vw;
+  height: 70vh;
+  padding: 10px;
+  background: rgba(36,46, 84);
+  border-radius: 2rem;
+  overflow: hidden;
+  box-shadow: 9px 10px 98px -22px rgba(0,0,0,0.75);
+  -webkit-box-shadow: 9px 10px 98px -22px rgba(0,0,0,0.75);
+  -moz-box-shadow: 9px 10px 98px -22px rgba(0,0,0,0.75);
+`
+
+const ResultMapContent = styled.div`
+  display: flex;
+  position: absolute;
+  height: 50%;
+  bottom: 50%;
+`
+
+const ResultRankingContent = styled.div`
+  display: flex;
+  position: absolute;
+  justify-content: space-between;
+  height: 50%;
+  bottom: 0;
+`
