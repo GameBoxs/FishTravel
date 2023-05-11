bfd20c551c9a4c72b63e3f2497f0b36f447d6087
cloudsoswift <magic9282m@gmail.com>
2023-05-11 17:50:05 +0900

    feat: Zustand로 게임 관련 설정 관리하도록 함.

--- frontend/src/present/pages/MultiGamePage.tsx
+++ frontend/src/present/pages/MultiGamePage.tsx
@@ -1,12 +1,30 @@
 import { useState } from 'react';
 import { create } from 'zustand';
 import { MultiGameLoading } from '../layout/multi/MultiGameLoading';
-import { MultiGameResult } from '../layout/multi/MultiGameResult';
+import { LatLng, MultiGameResult } from '../layout/multi/MultiGameResult';
 import useLoadScript from '../../action/hooks/useLoadScript';
 import { MultiGameProgress } from '../layout/multi/MultiGameProgress';
 type Props = {
   
 };
+type TGameSetting = {
+  gameStage: number;
+  isDomestic: boolean;
+  selectedPosition: LatLng;
+  isLoaded: string;
+  setGameStage: (value: number) => void;
+  setIsDomestic: (value: boolean) => void;
+  setIsLoaded: (value: string) => void;
+}
+export const useGameSettingStore = create<TGameSetting>((set, get) => ({
+  gameStage: 2,
+  isDomestic: false,
+  selectedPosition: { lat: 22, lng: 22 },
+  isLoaded: "loading",
+  setGameStage: (value: number) => set((state) => ({...state, gameStage: value})),
+  setIsDomestic: (value: boolean) => set((state) => ({ ...state, isDomestic: value })),
+  setIsLoaded: (value: string) => set((state) => ({ ...state, isLoaded: value })),
+}))
 export const MultiGamePage = (props: Props) => {
   //로직
   //페이지 진입
@@ -14,19 +32,15 @@ export const MultiGamePage = (props: Props) => {
   //1-1) 게임에 포함된 사람인지 확인
   //1-2) 게임에 포함된 경우, 죽은 사람 화면 띄워줌
   //2) 게임 
-  const useGameSetting = create((set, get) => ({
-    gameStage: 1,
-    isDomestic: false,
-    isLoaded: useLoadScript(get().isDomestic ? "https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=4vgyzjsnlj&submodules=panorama"
-    : "https://maps.googleapis.com/maps/api/js?key=AIzaSyC5fl-yV_BZhfIXZYDpU4JnCwFGDhd8oQA"),
-    setGameStage: (value: number) => set((state) => ({...state, gameStage: value})),
-    setIsDomestic: (value: boolean) => set((state) => ({...state, isDomestic: value})),
-  }))
+  const { gameStage, isDomestic, isLoaded, setIsLoaded } = useGameSettingStore();
+  const isLoadedState = useLoadScript(isDomestic ? "https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=4vgyzjsnlj&submodules=panorama"
+    : "https://maps.googleapis.com/maps/api/js?key=AIzaSyC5fl-yV_BZhfIXZYDpU4JnCwFGDhd8oQA");
+  console.log(isLoadedState);
   return (
     <div>
       {gameStage === 1 && <MultiGameLoading />}
-      {gameStage === 2 && <MultiGameProgress isDomestic={isDomestic} isLoaded={isLoaded} isObserver={false} /> }
-      {gameStage === 3 && <MultiGameResult isDomestic={isDomestic} isLoaded={isLoaded} /> }
+      {gameStage === 2 && <MultiGameProgress isDomestic={isDomestic} isLoaded={isLoadedState} isObserver={false} /> }
+      {gameStage === 3 && <MultiGameResult isDomestic={isDomestic} isLoaded={isLoadedState} /> }
     </div>
   );
 };
