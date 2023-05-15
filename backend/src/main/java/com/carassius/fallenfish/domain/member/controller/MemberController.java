bab1bf6814350a19c6ce54015d3a278c78be87ba
이창민 <cmlee0913@naver.com>
2023-05-15 22:42:31 +0900

    feat: 로그인 기능 구현

--- backend/src/main/java/com/carassius/fallenfish/domain/member/controller/MemberController.java
+++ backend/src/main/java/com/carassius/fallenfish/domain/member/controller/MemberController.java
@@ -1,22 +1,42 @@
 package com.carassius.fallenfish.domain.member.controller;
 
+import com.carassius.fallenfish.domain.member.entity.Member;
 import com.carassius.fallenfish.domain.member.service.MemberService;
+import com.carassius.fallenfish.security.LoginUser;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "회원 관리")
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class MemberController {
     @Value("${app.auth.access-token-name}")
     private String accessTokenName;
+
+    private final MemberService memberService;
+
+    @Operation(summary = "유저 정보 가져오기")
+    @GetMapping("/info")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity getUserInfo(@AuthenticationPrincipal LoginUser loginUser) {
+
+        log.info(loginUser.getId().toString());
+
+        return ResponseEntity.ok()
+            .header(null)
+            .body(memberService.getMember(loginUser.getId()));
+    }
 
     @Operation(summary = "로그아웃")
     @PostMapping("/logout")
--- backend/src/main/java/com/carassius/fallenfish/domain/member/service/MemberService.java
+++ backend/src/main/java/com/carassius/fallenfish/domain/member/service/MemberService.java
@@ -1,20 +1,38 @@
 package com.carassius.fallenfish.domain.member.service;
 
+import com.carassius.fallenfish.common.exception.ResourceNotFoundException;
 import com.carassius.fallenfish.domain.member.dto.MemberCreateRequest;
+import com.carassius.fallenfish.domain.member.dto.MemberInfoResponse;
 import com.carassius.fallenfish.domain.member.entity.Member;
 import com.carassius.fallenfish.domain.member.repository.MemberRepository;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Slf4j
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
+
+    @Transactional
+    public MemberInfoResponse getMember(Long userId) {
+        Member findMember = memberRepository.findById(userId).get();
+
+        MemberInfoResponse memberInfoResponse =
+            MemberInfoResponse.builder()
+                .loginId(findMember.getLoginId())
+                .name(findMember.getName())
+                .build();
+
+        return memberInfoResponse;
+    }
     @Transactional
     public Member createMember(MemberCreateRequest dto) {
         Member user = Member.builder()
