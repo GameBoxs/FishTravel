de7d4a176a710eb1cad512b6f5f3f5f1d5737df5
cloudsoswift <magic9282m@gmail.com>
2023-05-19 05:52:54 +0900

    feat: 게임 - 멀티 문제 출제자, 옵저버용 마커 배열, 랭킹 배열 및 해당 요소들 값 업데이트하는 함수 추가. 이전의 Store 삭제. 최종 결과창 반영

--- frontend/src/present/pages/MultiGamePage.tsx
+++ frontend/src/present/pages/MultiGamePage.tsx
@@ -8,26 +8,22 @@ import { MultiGameProgress } from '../layout/multi/MultiGameProgress';
 import { MultiGameLobby } from '../layout/multi/MultiGameLobby';
 
 import { MultiGamePicker } from '../layout/multi/MultiGamePicker';
-import { TGameInfo, TMessageCode, TPlayer, TRound, TBroadcastMessage, TMarkerRequest, TRanking } from './index.d';
-import { useParams } from 'react-router-dom';
+import { TGameInfo, TMessageCode, TPlayer, TRoundInfo, TBroadcastMessage, TMarkerRequest, TRanking } from './index.d';
+import { useNavigate, useParams } from 'react-router-dom';
 import { useUserStore } from '../../store/userStore';
 import { Client, Message } from '@stomp/stompjs';
 import GameConnect from '../component/gamelobby/GameConnect';
 import { Loading } from '../layout/multi/Loading';
 import { MultiWaitingPage } from '../layout/multi/MultiWaitingPage';
+import { MultiGameFinalResult } from '../layout/multi/MultiGameFinalResult';
 type Props = {};
-type TGameSetting = {
-  gameStage: number;
-  isDomestic: boolean;
-  selectedPosition: LatLng;
-  setGameStage: (value: number) => void;
-  setIsDomestic: (value: boolean) => void;
-};
 type TGameData = TGameInfo & {
   isObserver: boolean;
   problemPosition: TMarkerRequest | null;
+  problemRequester: TPlayer | null;
   submittedPosition: TMarkerRequest | null;
-  rankingArray: Array<TRanking> | null;
+  observingMarkerArray: Array<TMarkerRequest>;
+  rankingArray: Array<TRanking>;
   setGameInfo: (value: TGameInfo) => void;
   setRoomId: (value: string) => void;
   setCode: (value: TMessageCode) => void;
@@ -35,21 +31,17 @@ type TGameData = TGameInfo & {
   setManagerId: (value: number) => void;
   setMaxPlayers: (value: number) => void;
   setPlayers: (value: Array<TPlayer>) => void;
-  setRounds: (value: Array<TRound>) => void;
+  setRounds: (value: Array<TRoundInfo>) => void;
   setIsObserver: (value: boolean) => void;
   setProblemPosition: (value: TMarkerRequest) => void;
+  setProblemRequester: (value: TPlayer) => void;
   setSubmittedPosition: (value: TMarkerRequest) => void;
   setRankingArray: (value: Array<TRanking>) => void;
   refreshRanking: () => void;
+  updateIsObserver: () => void;
+  initializeRanking: () => void;
+  updateObservingMarkerArray: (value: TMarkerRequest) => void;
 };
-
-export const useGameSettingStore = create<TGameSetting>((set, get) => ({
-  gameStage: 0,
-  isDomestic: true,
-  selectedPosition: { nickname: '나', lat: 22, lng: 22 },
-  setGameStage: (value: number) => set((state) => ({ ...state, gameStage: value })),
-  setIsDomestic: (value: boolean) => set((state) => ({ ...state, isDomestic: value })),
-}));
 
 export const useGameInfoStore = create<TGameData>((set, get) => ({
   code: null,
@@ -57,12 +49,15 @@ export const useGameInfoStore = create<TGameData>((set, get) => ({
   managerId: 1,
   maxPlayers: 6,
   players: [],
+  currentRound: 0,
   roomId: '',
   rounds: [],
   isObserver: false,
   problemPosition: null,
+  problemRequester: null,
   submittedPosition: null,
-  rankingArray: null,
+  observingMarkerArray: [],
+  rankingArray: [],
   setGameInfo: (value: TGameInfo) => set((state) => ({ ...state, ...value })),
   setRoomId: (value: string) => set((state) => ({ ...state, roomId: value })),
   setCode: (value: TMessageCode) => set((state) => ({ ...state, code: value })),
@@ -70,18 +65,33 @@ export const useGameInfoStore = create<TGameData>((set, get) => ({
   setManagerId: (value: number) => set((state) => ({ ...state, managerId: value })),
   setMaxPlayers: (value: number) => set((state) => ({ ...state, maxPlayers: value })),
   setPlayers: (value: Array<TPlayer>) => set((state) => ({ ...state, players: value })),
-  setRounds: (value: Array<TRound>) => set((state) => ({ ...state, round: value })),
+  setRounds: (value: Array<TRoundInfo>) => set((state) => ({ ...state, round: value })),
   setIsObserver: (value: boolean) => set((state) => ({ ...state, isObserver: value })),
   setProblemPosition: (value: TMarkerRequest) => set((state) => ({ ...state, problemPosition: value })),
-  setSubmittedPosition: (value: TMarkerRequest) => set((state) => ({ ...state, ...(Number(useUserStore.getState().id) === value.requester.id && {code: "WAITING" as TMessageCode, submittedPosition: value}) })),
+  setProblemRequester: (value: TPlayer) => set((state) => ({ ...state, problemRequester: value })),
+  setSubmittedPosition: (value: TMarkerRequest) => set((state) => ({ ...state, ...(Number(useUserStore.getState().id) === value.requester.id && { code: "WAITING" as TMessageCode, submittedPosition: value }) })),
   setRankingArray: (value: Array<TRanking>) => set((state) => ({ ...state, rankingArray: value })),
+  updateIsObserver: () => set((state) => ({ ...state, isObserver: Number(useUserStore.getState().id) === get().problemRequester!.id })),
+  initializeRanking: () => set((state) => ({
+    ...state,
+    rankingArray: state.players!.map((p) => ({ player: p, scoreSum: 0 })),
+  })),
   refreshRanking: () => set((state) => ({
     ...state,
     rankingArray: state.rankingArray!.map((r) => ({
       ...r,
-      scoreSum: r.scoreSum + state.rounds!.at(-1)!.scores.filter((s) => s.answer.requester.id === r.player.id)[0]?.distance,
-    })).sort((a, b) => a.scoreSum < b.scoreSum ? 1 : a.scoreSum === b.scoreSum ? 0 : -1 ),
-  }))
+      scoreSum: r.scoreSum + state.rounds!.find((r) => r.roundOrder == get().currentRound)!.scores.filter((s) => s.answer.requester.id === r.player.id)[0]?.distance,
+    })).sort((a, b) => a.scoreSum < b.scoreSum ? -1 : a.scoreSum === b.scoreSum ? 0 : 1),
+  })),
+  updateObservingMarkerArray: (value: TMarkerRequest) => set((state) => ({
+    ...state,
+    ...(state.observingMarkerArray && state.isObserver &&
+    {
+      observingMarkerArray: state.observingMarkerArray.find((m) => m.requester.id === value.requester.id) ?
+        state.observingMarkerArray.map((m) => m.requester.id === value.requester.id ? value : m) :
+        [...state.observingMarkerArray, value]
+    })
+    }))
 }));
 export const MultiGamePage = (props: Props) => {
   const gameInfo = useGameInfoStore();
@@ -91,30 +101,31 @@ export const MultiGamePage = (props: Props) => {
       : 'https://maps.googleapis.com/maps/api/js?key=AIzaSyC5fl-yV_BZhfIXZYDpU4JnCwFGDhd8oQA',
   );
   const params = useParams();
-  const { id } = useUserStore();
+  const navigate = useNavigate();
+  const id = useUserStore((state) => state.id);
   const [postMessage, setPostMessage] = useState<any>('');
   const callback = (message: Message) => {
+    if (message.body === "다 끝났다") { 
+      gameInfo.setCode(TMessageCode.FINAL_RESULT);
+    }
     const body = JSON.parse(message.body);
     console.log(body);
-    console.log(id);
-    if (body.code ==="ANSWER") { 
+    if (body.code === "ANSWER") { 
+      gameInfo.updateObservingMarkerArray(body.data);
       gameInfo.setSubmittedPosition(body.data);
     }
     if (body.code === 'CHAT' && gameInfo.players !== null) {
       setPostMessage(body.data);
     }
     if (body.code === 'GAME_START' && gameInfo.players !== null) {
-      gameInfo.setRankingArray(gameInfo.players.map((p) => ({ player: p, scoreSum: 0 })));
+      gameInfo.initializeRanking();
     }
     if (body.code === 'IN_ROUND') {
       gameInfo.setCode(TMessageCode.IN_ROUND);
       const data: TMarkerRequest = body.data;
       gameInfo.setProblemPosition(data);
-      if (data.requester.id === Number(id)) {
-        gameInfo.setIsObserver(true);
-      } else {
-        gameInfo.setIsObserver(false);
-      }
+      gameInfo.setProblemRequester(data.requester);
+      gameInfo.updateIsObserver();
     } else {
       gameInfo.setGameInfo((JSON.parse(message.body) as TBroadcastMessage<TGameInfo>).data);
     }
@@ -125,8 +136,13 @@ export const MultiGamePage = (props: Props) => {
     }
   };
   useEffect(() => {
+    // if (!id) { 
+    //   alert('로그인이 필요합니다.')
+    //   navigate('/');
+    // }
     if (!params.roomCode) {
       alert('방 번호가 입력되지 않았습니다.');
+      navigate('/');
     }
   }, []);
   useEffect(() => {
@@ -148,7 +164,11 @@ export const MultiGamePage = (props: Props) => {
       )}
       {gameInfo.code === TMessageCode.WAITING && (
         <MultiWaitingPage isLoaded={isLoadedState} />
-      ) }
+      )}
+      {gameInfo.code === TMessageCode.FINAL_RESULT && (
+        <MultiGameFinalResult />
+      )
+      }
     </div>
   );
 };
