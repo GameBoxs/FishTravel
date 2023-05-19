838ef571c6edb18251f773ed48cefd16417981e0
임수민 <ddk03121@naver.com>
2023-05-19 10:28:33 +0900

    fix: rabbitmq

--- backend/src/main/java/com/carassius/fallenfish/config/WebSocketConfigurer.java
+++ backend/src/main/java/com/carassius/fallenfish/config/WebSocketConfigurer.java
@@ -1,6 +1,7 @@
 package com.carassius.fallenfish.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -12,14 +13,18 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @RequiredArgsConstructor
 public class WebSocketConfigurer implements WebSocketMessageBrokerConfigurer {
 
+    @Value("${spring.rabbitmq.host")
+    private String host;
+    @Value("${spring.rabbitmq.port")
+    private int port;
+
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/pub", "/topic")
                 .enableStompBrokerRelay("/topic")
-                .setRelayHost("localhost")
-//                .setRelayHost("rabbitmq")
+                .setRelayHost(host)
                 .setVirtualHost("/")
-                .setRelayPort(61613)
+                .setRelayPort(port)
                 .setClientLogin("guest")
                 .setClientPasscode("guest");
     }
